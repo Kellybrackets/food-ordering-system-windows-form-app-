ec1b78324c2c054708a68806613e849e50016b7e
Keletso Ntseno <ziginps@gmail.com>
2023-11-11 21:31:53 +0200

    first commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,331 @@
+﻿namespace TUT8_question2
+{
+    partial class lbTotal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(lbTotal));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.rbsmall = new System.Windows.Forms.RadioButton();
+            this.rbmedium = new System.Windows.Forms.RadioButton();
+            this.rblarge = new System.Windows.Forms.RadioButton();
+            this.rbthick = new System.Windows.Forms.RadioButton();
+            this.rbthin = new System.Windows.Forms.RadioButton();
+            this.cbpep = new System.Windows.Forms.CheckBox();
+            this.cbja = new System.Windows.Forms.CheckBox();
+            this.cbspi = new System.Windows.Forms.CheckBox();
+            this.cbSB = new System.Windows.Forms.CheckBox();
+            this.cbPine = new System.Windows.Forms.CheckBox();
+            this.cbEC = new System.Windows.Forms.CheckBox();
+            this.cbGB = new System.Windows.Forms.CheckBox();
+            this.cbDS = new System.Windows.Forms.CheckBox();
+            this.cbMush = new System.Windows.Forms.CheckBox();
+            this.lbthetotal = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(153, 94);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // rbsmall
+            // 
+            this.rbsmall.AutoSize = true;
+            this.rbsmall.Location = new System.Drawing.Point(32, 14);
+            this.rbsmall.Name = "rbsmall";
+            this.rbsmall.Size = new System.Drawing.Size(118, 24);
+            this.rbsmall.TabIndex = 2;
+            this.rbsmall.TabStop = true;
+            this.rbsmall.Text = "small (R 20)";
+            this.rbsmall.UseVisualStyleBackColor = true;
+            this.rbsmall.CheckedChanged += new System.EventHandler(this.rbsmall_CheckedChanged);
+            // 
+            // rbmedium
+            // 
+            this.rbmedium.AutoSize = true;
+            this.rbmedium.Location = new System.Drawing.Point(232, 14);
+            this.rbmedium.Name = "rbmedium";
+            this.rbmedium.Size = new System.Drawing.Size(138, 24);
+            this.rbmedium.TabIndex = 3;
+            this.rbmedium.TabStop = true;
+            this.rbmedium.Text = "medium (R 35)";
+            this.rbmedium.UseVisualStyleBackColor = true;
+            this.rbmedium.CheckedChanged += new System.EventHandler(this.rbmedium_CheckedChanged);
+            // 
+            // rblarge
+            // 
+            this.rblarge.AutoSize = true;
+            this.rblarge.Location = new System.Drawing.Point(426, 14);
+            this.rblarge.Name = "rblarge";
+            this.rblarge.Size = new System.Drawing.Size(123, 24);
+            this.rblarge.TabIndex = 4;
+            this.rblarge.TabStop = true;
+            this.rblarge.Text = "Large (R 55)";
+            this.rblarge.UseVisualStyleBackColor = true;
+            this.rblarge.CheckedChanged += new System.EventHandler(this.rblarge_CheckedChanged);
+            // 
+            // rbthick
+            // 
+            this.rbthick.AutoSize = true;
+            this.rbthick.Location = new System.Drawing.Point(78, 9);
+            this.rbthick.Name = "rbthick";
+            this.rbthick.Size = new System.Drawing.Size(106, 24);
+            this.rbthick.TabIndex = 5;
+            this.rbthick.TabStop = true;
+            this.rbthick.Text = "thick (R 2)";
+            this.rbthick.UseVisualStyleBackColor = true;
+            this.rbthick.CheckedChanged += new System.EventHandler(this.rbthick_CheckedChanged);
+            // 
+            // rbthin
+            // 
+            this.rbthin.AutoSize = true;
+            this.rbthin.Location = new System.Drawing.Point(254, 9);
+            this.rbthin.Name = "rbthin";
+            this.rbthin.Size = new System.Drawing.Size(99, 24);
+            this.rbthin.TabIndex = 6;
+            this.rbthin.TabStop = true;
+            this.rbthin.Text = "thin (R 1)";
+            this.rbthin.UseVisualStyleBackColor = true;
+            this.rbthin.CheckedChanged += new System.EventHandler(this.rbthin_CheckedChanged);
+            // 
+            // cbpep
+            // 
+            this.cbpep.AutoSize = true;
+            this.cbpep.Location = new System.Drawing.Point(65, 325);
+            this.cbpep.Name = "cbpep";
+            this.cbpep.Size = new System.Drawing.Size(97, 24);
+            this.cbpep.TabIndex = 7;
+            this.cbpep.Text = "peperoni";
+            this.cbpep.UseVisualStyleBackColor = true;
+            this.cbpep.CheckedChanged += new System.EventHandler(this.cbpep_CheckedChanged);
+            // 
+            // cbja
+            // 
+            this.cbja.AutoSize = true;
+            this.cbja.Location = new System.Drawing.Point(244, 325);
+            this.cbja.Name = "cbja";
+            this.cbja.Size = new System.Drawing.Size(95, 24);
+            this.cbja.TabIndex = 8;
+            this.cbja.Text = "jalapeno";
+            this.cbja.UseVisualStyleBackColor = true;
+            this.cbja.CheckedChanged += new System.EventHandler(this.cbja_CheckedChanged);
+            // 
+            // cbspi
+            // 
+            this.cbspi.AutoSize = true;
+            this.cbspi.Location = new System.Drawing.Point(414, 324);
+            this.cbspi.Name = "cbspi";
+            this.cbspi.Size = new System.Drawing.Size(90, 24);
+            this.cbspi.TabIndex = 9;
+            this.cbspi.Text = "spinach";
+            this.cbspi.UseVisualStyleBackColor = true;
+            this.cbspi.CheckedChanged += new System.EventHandler(this.cbspi_CheckedChanged);
+            // 
+            // cbSB
+            // 
+            this.cbSB.AutoSize = true;
+            this.cbSB.Location = new System.Drawing.Point(414, 377);
+            this.cbSB.Name = "cbSB";
+            this.cbSB.Size = new System.Drawing.Size(160, 24);
+            this.cbSB.TabIndex = 12;
+            this.cbSB.Text = "shredded chicken";
+            this.cbSB.UseVisualStyleBackColor = true;
+            this.cbSB.CheckedChanged += new System.EventHandler(this.cbSB_CheckedChanged);
+            // 
+            // cbPine
+            // 
+            this.cbPine.AutoSize = true;
+            this.cbPine.Location = new System.Drawing.Point(244, 377);
+            this.cbPine.Name = "cbPine";
+            this.cbPine.Size = new System.Drawing.Size(105, 24);
+            this.cbPine.TabIndex = 11;
+            this.cbPine.Text = "Pineapple";
+            this.cbPine.UseVisualStyleBackColor = true;
+            this.cbPine.CheckedChanged += new System.EventHandler(this.cbPine_CheckedChanged);
+            // 
+            // cbEC
+            // 
+            this.cbEC.AutoSize = true;
+            this.cbEC.Location = new System.Drawing.Point(65, 377);
+            this.cbEC.Name = "cbEC";
+            this.cbEC.Size = new System.Drawing.Size(131, 24);
+            this.cbEC.TabIndex = 10;
+            this.cbEC.Text = "Extra Cheese";
+            this.cbEC.UseVisualStyleBackColor = true;
+            this.cbEC.CheckedChanged += new System.EventHandler(this.cbEC_CheckedChanged);
+            // 
+            // cbGB
+            // 
+            this.cbGB.AutoSize = true;
+            this.cbGB.Location = new System.Drawing.Point(414, 430);
+            this.cbGB.Name = "cbGB";
+            this.cbGB.Size = new System.Drawing.Size(121, 24);
+            this.cbGB.TabIndex = 15;
+            this.cbGB.Text = "ground beef";
+            this.cbGB.UseVisualStyleBackColor = true;
+            this.cbGB.CheckedChanged += new System.EventHandler(this.cbGB_CheckedChanged);
+            // 
+            // cbDS
+            // 
+            this.cbDS.AutoSize = true;
+            this.cbDS.Location = new System.Drawing.Point(244, 431);
+            this.cbDS.Name = "cbDS";
+            this.cbDS.Size = new System.Drawing.Size(121, 24);
+            this.cbDS.TabIndex = 14;
+            this.cbDS.Text = "dried shrimp";
+            this.cbDS.UseVisualStyleBackColor = true;
+            this.cbDS.CheckedChanged += new System.EventHandler(this.cbDS_CheckedChanged);
+            // 
+            // cbMush
+            // 
+            this.cbMush.AutoSize = true;
+            this.cbMush.Location = new System.Drawing.Point(65, 431);
+            this.cbMush.Name = "cbMush";
+            this.cbMush.Size = new System.Drawing.Size(110, 24);
+            this.cbMush.TabIndex = 13;
+            this.cbMush.Text = "Mushroom";
+            this.cbMush.UseVisualStyleBackColor = true;
+            this.cbMush.CheckedChanged += new System.EventHandler(this.cbMush_CheckedChanged);
+            // 
+            // lbthetotal
+            // 
+            this.lbthetotal.AutoSize = true;
+            this.lbthetotal.Location = new System.Drawing.Point(111, 483);
+            this.lbthetotal.Name = "lbthetotal";
+            this.lbthetotal.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.lbthetotal.Size = new System.Drawing.Size(103, 20);
+            this.lbthetotal.TabIndex = 16;
+            this.lbthetotal.Text = "Amount Due:";
+            this.lbthetotal.Click += new System.EventHandler(this.lbthetotal_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 45F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(187, 4);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(436, 102);
+            this.label1.TabIndex = 17;
+            this.label1.Text = "Pizza Hut";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Location = new System.Drawing.Point(12, 284);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(594, 196);
+            this.groupBox1.TabIndex = 18;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Topping Each @ R7";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rbthin);
+            this.panel1.Controls.Add(this.rbthick);
+            this.panel1.Location = new System.Drawing.Point(12, 213);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(611, 47);
+            this.panel1.TabIndex = 19;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.rblarge);
+            this.panel2.Controls.Add(this.rbmedium);
+            this.panel2.Controls.Add(this.rbsmall);
+            this.panel2.Location = new System.Drawing.Point(12, 138);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(611, 51);
+            this.panel2.TabIndex = 20;
+            // 
+            // lbTotal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(670, 521);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbthetotal);
+            this.Controls.Add(this.cbGB);
+            this.Controls.Add(this.cbDS);
+            this.Controls.Add(this.cbMush);
+            this.Controls.Add(this.cbSB);
+            this.Controls.Add(this.cbPine);
+            this.Controls.Add(this.cbEC);
+            this.Controls.Add(this.cbspi);
+            this.Controls.Add(this.cbja);
+            this.Controls.Add(this.cbpep);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.panel2);
+            this.Name = "lbTotal";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton rbsmall;
+        private System.Windows.Forms.RadioButton rbmedium;
+        private System.Windows.Forms.RadioButton rblarge;
+        private System.Windows.Forms.RadioButton rbthick;
+        private System.Windows.Forms.RadioButton rbthin;
+        private System.Windows.Forms.CheckBox cbpep;
+        private System.Windows.Forms.CheckBox cbja;
+        private System.Windows.Forms.CheckBox cbspi;
+        private System.Windows.Forms.CheckBox cbSB;
+        private System.Windows.Forms.CheckBox cbPine;
+        private System.Windows.Forms.CheckBox cbEC;
+        private System.Windows.Forms.CheckBox cbGB;
+        private System.Windows.Forms.CheckBox cbDS;
+        private System.Windows.Forms.CheckBox cbMush;
+        private System.Windows.Forms.Label lbthetotal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TUT8_question2
+{
+    public partial class lbTotal : Form
+    {
+        public lbTotal()
+        {
+            InitializeComponent();
+        }
+
+        float total = 0;
+        private void cbpep_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbpep.Checked)
+                total += 7;
+            else
+                total -= 7;
+            lbthetotal.Text = "Amount Due: " + total.ToString();
+        }
+
+        private void cbja_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbja.Checked)
+                total += 7;
+            else
+                total -= 7;
+            lbthetotal.Text = "Amount Due: " + total.ToString();
+        }
+
+        private void cbspi_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbspi.Checked)
+                total += 7;
+            else
+                total -= 7;
+            lbthetotal.Text = "Amount Due: " + total.ToString();
+        }
+
+        private void cbEC_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbEC.Checked)
+                total += 7;
+            else
+                total -= 7;
+            lbthetotal.Text = "Amount Due: " + total.ToString();
+        }
+
+        private void cbPine_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbPine.Checked)
+                total += 7;
+            else
+                total -= 7;
+            lbthetotal.Text = "Amount Due: " + total.ToString();
+        }
+
+        private void cbSB_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbSB.Checked)
+                total += 7;
+            else
+                total -= 7;
+            lbthetotal.Text = "Amount Due: " + total.ToString();
+        }
+
+        private void cbMush_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbMush.Checked)
+                total += 7;
+            else
+                total -= 7;
+            lbthetotal.Text = "Amount Due: " + total.ToString();
+        }
+
+        private void cbDS_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbDS.Checked)
+                total += 7;
+            else
+                total -= 7;
+            lbthetotal.Text = "Amount Due: " + total.ToString();
+        }
+
+        private void cbGB_CheckedChanged(object sender, EventArgs e)
+        {
+            if (cbGB.Checked)
+                total += 7;
+            else
+                total -= 7;
+            lbthetotal.Text = "Amount Due: " + total.ToString();
+
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rbsmall_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbsmall.Checked)
+                total += 20;
+            else
+                total -= 20;
+            lbthetotal.Text = "Amount Due: " + total.ToString();
+
+        }
+
+        private void rbmedium_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbmedium.Checked)
+                total += 35;
+            else
+                total -= 35;
+            lbthetotal.Text = "Amount Due: " + total.ToString();
+        }
+
+        private void rblarge_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rblarge.Checked)
+                total += 55;
+            else
+                total -= 55;
+            lbthetotal.Text = "Amount Due: " + total.ToString();
+        }
+
+        private void rbthick_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbthick.Checked)
+                total += 2;
+            else
+                total -= 2;
+            lbthetotal.Text = "Amount Due: " + total.ToString();
+        }
+
+        private void rbthin_CheckedChanged(object sender, EventArgs e)
+        {
+            if (rbthin.Checked)
+                total += 1;
+            else
+                total -= 1;
+            lbthetotal.Text = "Amount Due: " + total.ToString();
+        }
+
+        private void lbthetotal_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(total.ToString());
+        }
+    }
+}
